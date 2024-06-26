119a6f43eba6ef34e49b304d62f54833115dd62e
MCamish29 <mcamish29@gmail.com>
2024-06-26 17:53:29 +0100

    "Add multiplication game code"

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,11 +30,15 @@ function runGame(gameType){
     let num2 = Math.floor(Math.random() * 25)+1;
 
     if (gameType === "addition"){
-        displayAdditionQuestion(num1, num2)
+        displayAdditionQuestion(num1, num2);
+    } else if (gameType==="multiply"){
+        displayMultiplyQuestion(num1, num2);
     } else {
         alert(`Uknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
     }
+
+
 }
 
 /**
@@ -68,6 +72,8 @@ function calculateCorrectAnswer(){
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
+    } else if (operator === "x"){
+        return [operand1 * operand2, "multiply"];
     } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
@@ -100,8 +106,10 @@ function displaySubtractQuestion(){
 
 }
 
-function displayMultiplyQuestion(){
-
+function displayMultiplyQuestion(operand1, operand2){
+    document.getElementById("operand1").textContent = operand1;
+    document.getElementById("operand2").textContent = operand2;
+    document.getElementById("operator").textContent = "x";
 }
 
 function displayDivisionQuestion(){
